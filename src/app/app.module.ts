3a22c665b4292eb2f80d9a0ea7a10dd615440138
Carlos Ramirez <carlos.ramirez1@unmsm.edu.pe>
2017-07-05 12:43:59 -0500

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { LoginGuard } from './servicios/login-guard.service';
+import { AuthGuard } from './servicios/auth-guard.service';
+import { AuthService } from './servicios/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from "app/app-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
+import { HttpModule } from "@angular/http";
+import { CuposManagerComponent } from './cupos-manager/cupos-manager.component';
+import { CocheraService } from "app/servicios/cochera.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    CuposManagerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AuthService, CocheraService, LoginGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
